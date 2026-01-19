01992eb411b88d4a1e23bfa66ca88b0c24876d6a
minho-03 <pmh9038@naver.com>
2026-01-19 17:53:23 +0900

    Delete projects directory

--- projects/0119/projects/0119/readline_test.py
+++
@@ -1,8 +0,0 @@
-f = open("C:/Users/405/projects/0119/새파일.txt",'r')
-while True:
-    line = f.readline()
-    if not line:
-        break
-    print(line)
-f.close()
-    
--- projects/0119/write_data.py
+++
@@ -1,6 +0,0 @@
-f = open("C:/Users/405/projects/0119/새파일.txt",'w')
-for i in range(1,11):
-    data = "%d line of code.\n" % i
-    f.write(data)
-f.close()
-    
